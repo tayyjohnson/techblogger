bc634e2ace66dba3f1090dda53e9ccf9ca94abb9
tayyjohnson <taydriver1@gmail.com>
2022-10-15 12:12:19 -0400

    big changes & improvements!

--- server.js
+++ server.js
@@ -23,17 +23,13 @@ app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set('view engine', 'handlebars');
 
-// serve up public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//turn on routes
 app.use(routes);
 
-// turn on connection to db and server
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
